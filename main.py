cabc74c47c9fca994cb62256a80c0814b5bed3ef
Ofek <tupe12334@gmail.com>
2021-07-17 21:35:11 +0300

    ta

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import gym
-import random
-import numpy as np
-from tensorflow.keras.models import Sequential
+from keras.utils.vis_utils import plot_model
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import BoltzmannQPolicy
 from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from keras_visualizer import visualizer
 
 
 def main():
-    epochs = 10
     env = gym.make('CartPole-v0')
     states = env.observation_space.shape[0]
     actions = env.action_space.n
     model = build_model(states, actions)
     model.summary()
-    for epoch in range(1, epochs):
-        state = env.reset()
-        done = False
-        score = 0
-        while not done:
-            env.render()
-            action = random.choice([0, 1])
-            n_state, reward, done, info = env.step(action)
-            score += reward
-            print(f'Epoch:{epoch}, score:{score}')
+    plot_model(model, to_file='./model.png',
+               show_shapes=True, show_layer_names=True, save=True)
+
+    dqn = build_agent(model=model, actions=actions)
+
+    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+    dqn.load_weights('dqn_weights.h5f')
+    # dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
+    # scores = dqn.test(env, nb_episodes=100, visualize=False)
+    # print(np.mean(scores.history['episode_reward']))
+    _ = dqn.test(env, nb_episodes=15, visualize=True)
+    # dqn.save_weights('dqn_weights.h5f', overwrite=True)
 
 
 def build_model(states, actions):
@@ -32,9 +34,17 @@ def build_model(states, actions):
     model.add(Dense(24, activation='relu'))
     model.add(Dense(24, activation='relu'))
     model.add(Dense(actions, activation="linear"))
-    visualizer(model, format='png', view=True)
     return model
 
 
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory,
+                   policy=policy, nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+    return dqn
+
+
 if __name__ == '__main__':
+    print("running!")
     main()
